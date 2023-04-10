cc11483878f01885ae0cbb0cf4fff41b9a2a20c8
Stella Nthenya <stellaalogi@gmail.com>
2023-04-10 10:30:04 +0300

    Completed this lab assignment successfully

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+// return's a new array containing the first two drivers
+const returnFirstTwoDrivers = function () {
+    let newDrivers = drivers.slice(0,2);
+    return newDrivers;
+};
+
+returnFirstTwoDrivers(drivers);
+
+// return's a new array containing the last two drivers
+const returnLastTwoDrivers = function () {
+    let newDrivers2 = drivers.slice(-2);
+    return newDrivers2;
+};
+
+returnLastTwoDrivers(drivers);
+
+// array of the two functions
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// higher-order function 
+function createFareMultiplier (num) {
+    return function (fare) {
+        return fare * num
+    }
+}
+
+// is a function that doubles fares
+const fareDoubler = function (fare) {
+    return createFareMultiplier(2)(fare)
+}
+
+// is a function that triples fares
+const fareTripler = function (fare) {
+    return createFareMultiplier(3)(fare)
+}
+
+
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers);
+};
+
+
+// you can console.log to confirm
+selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+selectDifferentDrivers(drivers, returnLastTwoDrivers);
